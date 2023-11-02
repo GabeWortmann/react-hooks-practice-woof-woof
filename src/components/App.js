472c26df2d6e6417ecb8a0e747bed0c6726d7340
Gabriel Wortmann <wortmanngabriel@gmail.com>
2023-11-01 22:24:11 -0500

    late commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [pets, setPups] = useState([])
+
+  useEffect(() => {
+    const fetchPupData = () => {
+      fetch("http://localhost:3001/pups")
+        .then((res) => res.json())
+        .then((data) => {
+          setPups(data);
+        })
+        .catch((error) => {
+          console.error("Error fetching pup data:", error);
+        });
+    };
+
+   
+    fetchPupData();
+  }, []);
+
   return (
     <div className="App">
       <div id="filter-div">
         <button id="good-dog-filter">Filter good dogs: OFF</button>
       </div>
-      <div id="dog-bar"></div>
+      <div id="dog-bar">
+        {pets.map((pet) => (
+          <span key={pet.id} onClick={() => clickFunction()}>{pet.name}</span>
+        ))}
+      </div>
       <div id="dog-summary-container">
         <h1>DOGGO:</h1>
         <div id="dog-info"></div>
@@ -14,5 +36,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
